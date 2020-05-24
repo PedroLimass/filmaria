8dc6fa667f0d90c6fcc4bbb042722199ef98ff30
Pedro Lima <48936052+PedroLimass@users.noreply.github.com>
2020-05-24 19:38:51 -0300

    Adicionando Rotas para a pagina!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Routes from './routes';
 
-function App() {
-  return (
-    <div className='App'>
-      Filmaria
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className='App'>
+        <Routes/>
+      </div>
+    );
+  }
 }
 
 export default App;
